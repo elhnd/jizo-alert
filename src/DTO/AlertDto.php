d94e0de98a6b6c16b693d9aa3fec2b5ecdb393f0
elhadji <elhadji.ndiaye@sesame-it.fr>
2024-01-29 16:23:28 +0100

    add DTO class

---
+++ src/DTO/AlertDto.php
@@ -0,0 +1,28 @@
+<?php
+namespace Sesame\Bundle\JizoAlerts\DTO;
+readonly class AlertDto
+{
+    public function __construct(
+        public ?string $ruleSidRev = null,
+        public ?string $protocol = null,
+        public ?string $content = null,
+        public ?string $dateTimeFrom = null,
+        public ?string $dateTimeTo = null,
+        public ?string $srcIp = null,
+        public ?string $flowId = null,
+        public ?string $destIp = null,
+        public ?string $destPort = null,
+        public ?string $srcPort = null,
+        public ?string $signature = null,
+        public ?string $appProto = null,
+        public ?string $severity = null,
+        public ?string $state = null,
+        public ?string $alertCategory = null,
+        public ?string $investigationConclusion = null,
+        public ?string $flag = null,
+        public ?string $sidExcluded = null,
+        public ?string $vlanId = null,
+        public ?int    $offset = null,
+        public ?int    $limit = null
+    ){}
+}
